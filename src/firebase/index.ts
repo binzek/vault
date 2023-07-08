692c8097b2dfd45721aedb383373e515499cb93b
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-08 16:56:26 +0530

    feat: install and configure firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,16 @@
+// Library imports
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQSn7_Asvq9bwIlh3YxBTa6xFK3MLxDxM",
+  authDomain: "vault-911.firebaseapp.com",
+  projectId: "vault-911",
+  storageBucket: "vault-911.appspot.com",
+  messagingSenderId: "514646272717",
+  appId: "1:514646272717:web:13890cc3ad04f92dcdd784",
+  measurementId: "G-6LPHD5ZW37",
+};
+
+// Initialize firebase app
+const app = initializeApp(firebaseConfig);
