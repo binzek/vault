d66f4764a6447d741a9a07c47c6d34ae5318f8a4
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-08 15:22:50 +0530

    feat: setup basic home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,16 @@
+// Library imports
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to :VauLT:</h1>
+      <Link to="/create">Create Account</Link>
+      <br />
+      <Link to="/login">Log In</Link>
+    </div>
+  );
 };
 
 export default Home;
