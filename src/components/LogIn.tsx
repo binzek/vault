1973b50d878c08f4b71fda933c65f762deedacbb
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-08 15:54:05 +0530

    feat: setup basic login page

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,7 +1,35 @@
+// Library imports
 import { FC } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+// Form input fields' types
+interface FormInputs {
+  email: string;
+  password: string;
+}
 
 const LogIn: FC = () => {
-  return <div>LogIn</div>;
+  const { register, handleSubmit } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
+  return (
+    <div>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          <span>Email</span>
+          <input type="email" {...register("email")} />
+        </label>
+        <br />
+        <label>
+          <span>Password</span>
+          <input type="password" {...register("password")} />
+        </label>
+        <br />
+        <button type="submit">Continue</button>
+      </form>
+    </div>
+  );
 };
 
 export default LogIn;
