367e9ae52abf177937f0c45ad5a13fb7011f7073
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-08 15:46:17 +0530

    feat: setup basic create account page

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,7 +1,41 @@
+// Library imports
 import { FC } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+// Form input fields' types
+interface FormInputs {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const CreateAccount: FC = () => {
-  return <div>CreateAccount</div>;
+  const { register, handleSubmit } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
+  return (
+    <div>
+      <h1>Create Account</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          <span>Name</span>
+          <input type="text" {...register("name")} />
+        </label>
+        <br />
+        <label>
+          <span>Email</span>
+          <input type="email" {...register("email")} />
+        </label>
+        <br />
+        <label>
+          <span>Password</span>
+          <input type="password" {...register("password")} />
+        </label>
+        <br />
+        <button type="submit">Continue</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateAccount;
