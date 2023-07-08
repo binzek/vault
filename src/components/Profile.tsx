742281813b2d85c74296ab7846da383531be863c
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-08 23:20:49 +0530

    feat: style profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 
 // Local imports
 import { signOut } from "../utils/auth";
+import classes from "../styles/Profile.module.css";
 
 // Props for profile
 interface Props {
@@ -11,10 +12,26 @@ interface Props {
 
 const Profile: FC<Props> = ({ userName }) => {
   return (
-    <div>
-      <h1>Hello {userName}</h1>
-      <button onClick={signOut}>Sign Out</button>
-    </div>
+    <section className={`${classes.profile} container`}>
+      <div className={`logo`}>
+        <img src="/src/assets/icon.svg" alt="VauLT Logo" />
+        <span>:VauLT:</span>
+      </div>
+      <div className={`${classes.profile__text}`}>
+        <h1>Hello There, {userName}</h1>
+        <p>
+          A safe place to store, access, and manage {userName}'s data with ease.
+        </p>
+      </div>
+      <button
+        className={`${classes.signout__button}`}
+        onClick={() => {
+          confirm("Are you sure sign out from VauLT ?") && signOut();
+        }}
+      >
+        Sign Out
+      </button>
+    </section>
   );
 };
 
