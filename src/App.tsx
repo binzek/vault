c56a34e335aee43ad1bb161700d0d9960da0f3b0
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-09 07:54:35 +0530

    feat: add a watermark on bottom

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,16 @@ import { Home, CreateAccount, LogIn } from "./components";
 
 const App: FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create" element={<CreateAccount />} />
-      <Route path="/login" element={<LogIn />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateAccount />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+      <a href="https://wajid.me/" target="_blank" className="watermark">
+        &copy; 2023 - Abdul Wajid
+      </a>
+    </>
   );
 };
 
