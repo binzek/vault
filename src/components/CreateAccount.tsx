ddedd65681a62e5a39371001db185a1c386b4898
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-09 07:19:55 +0530

    feat: setup auto navigation

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,11 +1,13 @@
 // Library imports
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 // Local imports
-import { createAccount } from "../utils/auth";
+import { createAccount, auth } from "../utils/auth";
 import classes from "../styles/Form.module.css";
+import { useAppSelector } from "../app/hooks";
 
 // Form input fields' types
 interface FormInputs {
@@ -18,6 +20,12 @@ const CreateAccount: FC = () => {
   // Register react-hook-form
   const { register, handleSubmit } = useForm<FormInputs>();
 
+  // Get user's status
+  const { isUser } = useAppSelector((state) => state.user);
+
+  // Navigator
+  const navigate = useNavigate();
+
   // Submit handler function for input form
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     // Create a new user on form submit
@@ -26,57 +34,70 @@ const CreateAccount: FC = () => {
       email: data.email,
       password: data.password,
     });
+    navigate("/");
   };
 
-  return (
-    <div className={`${classes.form} container`}>
-      <div className={`logo`}>
-        <Link to="/">
-          <img src="/src/assets/icon.svg" alt="VauLT Logo" />
-          <span>:VauLT:</span>
-        </Link>
+  useEffect(() => {
+    isUser
+      ? navigate("/")
+      : onAuthStateChanged(auth, (user) => {
+          user && navigate("/");
+        });
+  }, []);
+
+  if (!isUser) {
+    return (
+      <div className={`${classes.form} container`}>
+        <div className={`logo`}>
+          <Link to="/">
+            <img src="/src/assets/icon.svg" alt="VauLT Logo" />
+            <span>:VauLT:</span>
+          </Link>
+        </div>
+        <h1 className={`${classes.form__heading}`}>Create A New Account</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className={`${classes.form__field}`}>
+            <span>Name</span>
+            <input
+              type="text"
+              placeholder="John Doe"
+              required
+              {...register("name")}
+            />
+          </label>
+          <label className={`${classes.form__field}`}>
+            <span>Email</span>
+            <input
+              type="email"
+              placeholder="johndoe@example.com"
+              required
+              {...register("email")}
+            />
+          </label>
+          <label className={`${classes.form__field}`}>
+            <span>Password</span>
+            <input
+              type="password"
+              placeholder="********"
+              required
+              {...register("password")}
+            />
+          </label>
+          <button
+            type="submit"
+            className={`${classes.form__button} button button-primary`}
+          >
+            Continue
+          </button>
+        </form>
+        <p className={`${classes.form__link}`}>
+          Already have an account? <Link to="/login">Log In</Link>
+        </p>
       </div>
-      <h1 className={`${classes.form__heading}`}>Create A New Account</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label className={`${classes.form__field}`}>
-          <span>Name</span>
-          <input
-            type="text"
-            placeholder="John Doe"
-            required
-            {...register("name")}
-          />
-        </label>
-        <label className={`${classes.form__field}`}>
-          <span>Email</span>
-          <input
-            type="email"
-            placeholder="johndoe@example.com"
-            required
-            {...register("email")}
-          />
-        </label>
-        <label className={`${classes.form__field}`}>
-          <span>Password</span>
-          <input
-            type="password"
-            placeholder="********"
-            required
-            {...register("password")}
-          />
-        </label>
-        <button
-          type="submit"
-          className={`${classes.form__button} button button-primary`}
-        >
-          Continue
-        </button>
-      </form>
-      <p className={`${classes.form__link}`}>
-        Already have an account? <Link to="/login">Log In</Link>
-      </p>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default CreateAccount;
--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,11 +1,13 @@
 // Library imports
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 // Local imports
-import { signIn } from "../utils/auth";
+import { auth, signIn } from "../utils/auth";
 import classes from "../styles/Form.module.css";
+import { useAppSelector } from "../app/hooks";
 
 // Form input fields' types
 interface FormInputs {
@@ -17,52 +19,73 @@ const LogIn: FC = () => {
   // Register react-hook-form
   const { register, handleSubmit } = useForm<FormInputs>();
 
+  // Get user's status
+  const { isUser } = useAppSelector((state) => state.user);
+
+  // Navigator
+  const navigate = useNavigate();
+
   // Submit handler function for input form
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     // Signin with the given data on form submit
     await signIn({ email: data.email, password: data.password });
+    navigate("/");
   };
 
-  return (
-    <div className={`${classes.form} container`}>
-      <div className={`logo`}>
-        <Link to="/">
-          <img src="/src/assets/icon.svg" alt="VauLT Logo" />
-          <span>:VauLT:</span>
-        </Link>
+  useEffect(() => {
+    isUser
+      ? navigate("/")
+      : onAuthStateChanged(auth, (user) => {
+          user && navigate("/");
+        });
+  }, []);
+
+  if (!isUser) {
+    return (
+      <div className={`${classes.form} container`}>
+        <div className={`logo`}>
+          <Link to="/">
+            <img src="/src/assets/icon.svg" alt="VauLT Logo" />
+            <span>:VauLT:</span>
+          </Link>
+        </div>
+        <h1 className={`${classes.form__heading}`}>
+          Log In To Existing Account
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className={`${classes.form__field}`}>
+            <span>Email</span>
+            <input
+              type="email"
+              placeholder="johndoe@example.com"
+              required
+              {...register("email")}
+            />
+          </label>
+          <label className={`${classes.form__field}`}>
+            <span>Password</span>
+            <input
+              type="password"
+              placeholder="********"
+              required
+              {...register("password")}
+            />
+          </label>
+          <button
+            type="submit"
+            className={`${classes.form__button} button button-primary`}
+          >
+            Continue
+          </button>
+        </form>
+        <p className={`${classes.form__link}`}>
+          Don't have an account? <Link to="/create">Create</Link>
+        </p>
       </div>
-      <h1 className={`${classes.form__heading}`}>Log In To Existing Account</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label className={`${classes.form__field}`}>
-          <span>Email</span>
-          <input
-            type="email"
-            placeholder="johndoe@example.com"
-            required
-            {...register("email")}
-          />
-        </label>
-        <label className={`${classes.form__field}`}>
-          <span>Password</span>
-          <input
-            type="password"
-            placeholder="********"
-            required
-            {...register("password")}
-          />
-        </label>
-        <button
-          type="submit"
-          className={`${classes.form__button} button button-primary`}
-        >
-          Continue
-        </button>
-      </form>
-      <p className={`${classes.form__link}`}>
-        Don't have an account? <Link to="/create">Create</Link>
-      </p>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default LogIn;
